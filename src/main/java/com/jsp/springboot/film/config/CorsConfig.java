31b6f529f95113642d3ea6445feb456b2a8eb5ec
Vivek Dhamanemath <vivekjdwork@gmail.com>
2025-02-22 14:21:27 +0530

    Update CorsConfig.java

--- src/main/java/com/jsp/springboot/film/config/CorsConfig.java
+++ src/main/java/com/jsp/springboot/film/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://artistryx-mtrpgr0n7-vivek-s-projects-78825531.vercel.app/login")
+                        .allowedOrigins("https://artistryx-qrahmtpsx-vivek-s-projects-78825531.vercel.app/login")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
